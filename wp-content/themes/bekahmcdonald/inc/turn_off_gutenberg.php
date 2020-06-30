3259c1db0dfe248202ac1b83714a35d72794895b
Bekah McDonald <github@bekahmcdonald.com>
2020-06-30 14:49:35 +0100

    Enable Gutenberg for default post type

--- wp-content/themes/bekahmcdonald/inc/turn_off_gutenberg.php
+++ wp-content/themes/bekahmcdonald/inc/turn_off_gutenberg.php
@@ -1,3 +1,16 @@
 <?php
 
-add_filter('use_block_editor_for_post', '__return_false', 10);
+// Disable completely
+// add_filter('use_block_editor_for_post', '__return_false', 10);
+
+
+// Disable per post type
+add_filter('use_block_editor_for_post_type', 'bm_disable_block_editor', 10, 2);
+
+function bm_disable_block_editor($use_block_editor, $post_type) {
+  if (!in_array($post_type, array('post'))) {
+    return false;
+  }
+
+  return $use_block_editor;
+}
